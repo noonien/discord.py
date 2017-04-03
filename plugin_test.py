efefa575762990f7bfb6cb7952395fd7b23d173b
Gabriel Ionescu <gabrielcristian.ionescu@gmail.com>
2017-04-03 10:06:37 +0300

    message hooks work, timer in progress

---
+++ plugin_test.py
@@ -0,0 +1,67 @@
+import os, re
+import discord
+import asyncio
+
+from plugin import PluginManager
+from cloudbot.event import Event, CommandEvent
+from bare_loader import plugin
+
+plugin_manager = PluginManager(None)
+plugin_manager.load_all(os.path.abspath("plugins"))
+
+command_prefix = "."
+
+client = discord.Client()
+
+async def polling_task():
+    global plugin_manager
+    await client.wait_until_ready()
+
+    channel = discord.Object(id='297483005763780613')
+    while not client.is_closed:
+        for name, plugin in plugin_manager.plugins.items():
+            for periodic in plugin.periodic:
+                event = Event(bot=None, hook=periodic)
+                plugin_manager.launch(periodic, event)
+
+        await asyncio.sleep(1)
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+        
+    tasks = []
+    event = Event()
+    event.content = message.content
+    event.chan = message.channel
+    event.conn = client
+    event.nick = message.author.mention
+    client.nick = client.user.name
+    
+    command_re = r'(?i)^(?:[{}]|{}[,;:]+\s+)(\w+)(?:$|\s+)(.*)'.format(command_prefix, event.nick)
+    cmd_match = re.match(command_re, event.content)
+    if cmd_match:
+        command = cmd_match.group(1).lower()
+        if command in plugin_manager.commands:
+            command_hook = plugin_manager.commands[command]
+            command_event = CommandEvent(hook=command_hook, text=cmd_match.group(2).strip(),
+                                     triggered_command=command, base_event=event)
+            tasks.append(plugin_manager.launch(command_hook, command_event))
+            await client.send_message(message.channel, event.reply_message)
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.loop.create_task(polling_task())
+
+client.run('Mjk3NDgzMTk1NjI3MzM5Nzc3.C8BcvA.VBAA0nMlD3iIPQdOdtPuuMi4kv0')
---
+++ plugins/system.py
@@ -0,0 +1,63 @@
+import os
+import time
+import platform
+from datetime import timedelta
+
+try:
+    import psutil
+except ImportError:
+    psutil = None
+
+from cloudbot import hook
+from cloudbot.util.filesize import size as format_bytes
+import cloudbot
+
+
+@hook.command(autohelp=False)
+def about(text, conn):
+    return "{} is powered by a bear on an unicycle.format(conn.nick)"
+
+
+@hook.command(autohelp=False)
+def system(reply, message):
+    """-- Retrieves information about the host system."""
+
+    # Get general system info
+    sys_os = platform.platform()
+    python_implementation = platform.python_implementation()
+    python_version = platform.python_version()
+    sys_architecture = '-'.join(platform.architecture())
+    sys_cpu_count = platform.machine()
+
+    reply(
+        "OS: \x02{}\x02, "
+        "Python: \x02{} {}\x02, "
+        "Architecture: \x02{}\x02 (\x02{}\x02)"
+        .format(
+            sys_os,
+            python_implementation,
+            python_version,
+            sys_architecture,
+            sys_cpu_count)
+    )
+
+    if psutil:
+        process = psutil.Process(os.getpid())
+
+        # get the data we need using the Process we got
+        cpu_usage = process.cpu_percent(1)
+        thread_count = process.num_threads()
+        memory_usage = format_bytes(process.memory_info()[0])
+        uptime = timedelta(seconds=round(time.time() - process.create_time()))
+
+        message(
+            "Uptime: \x02{}\x02, "
+            "Threads: \x02{}\x02, "
+            "CPU Usage: \x02{}\x02, "
+            "Memory Usage: \x02{}\x02"
+            .format(
+                uptime,
+                thread_count,
+                cpu_usage,
+                memory_usage)
+        )
